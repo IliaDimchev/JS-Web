3e1c4a157002f486ae90bd9e7e192b8e4045d6ca
ilko <ilkothetiger@gmail.com>
2023-01-23 07:14:30 +0000

    Development setup ready

--- cubicle/src/index.js
+++ cubicle/src/index.js
@@ -1,1 +1,10 @@
-console.log('initial')
+const express = require('express');
+const config = require('./config.js')
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Home page');
+});
+
+app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}`));

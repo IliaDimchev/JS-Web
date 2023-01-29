f20c4188ee7847148a3dc16e4b4b6276cb5acee0
ilko <ilkothetiger@gmail.com>
2023-01-29 16:49:52 +0000

    Cube model creted with cubeSchema exports

--- cubicle/src/index.js
+++ cubicle/src/index.js
@@ -16,4 +16,4 @@ app.use(routes);
 
 initDatabase()
     .then(() => app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}`)))
-    .catch((err) => console.error(err));
+    .catch((err) => console.error(err.message));
--- cubicle/src/models/Cube.js
+++ cubicle/src/models/Cube.js
@@ -1,4 +1,8 @@
-const mongoose = require('mongoose');
+const { mongoose, model } = require('mongoose');
+
+// Short-Way 
+// const { Schema } = require('mongoose');
+// const cubeSchema = new Schema;
 
 const cubeSchema = new mongoose.Schema({
     name: {
@@ -22,30 +26,6 @@ const cubeSchema = new mongoose.Schema({
     }
 });
 
-// Short-Way 
-// const { Schema } = require('mongoose');
-// const cubeSchema = new Schema;
-
-
-
-
-class Cube {
-    constructor({name, description, imageUrl, difficultyLevel}) {
-        this.name = name, 
-        this.description = description, 
-        this.imageUrl = imageUrl, 
-        this.difficultyLevel = difficultyLevel
-    }
-
-    //Validations possible
-
-    static save(cube) {
-        this.id = db.cubes[db.cubes.length - 1].id + 1;
-        // this.id = db.cubes.length; - might overlap IDs, if some are deleted
-        db.cubes.push(cube);
-        let json = JSON.stringify(db, null, 2);
-        fs.writeFileSync(path.resolve(__dirname, '../db.json'), json);
-    }
-}
+const Cube = model('Cube', cubeSchema);
 
 module.exports = Cube;

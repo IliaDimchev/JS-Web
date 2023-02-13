93bc5f9984c9ffdf2a5a9206d6a4b470073d530c
ilko <ilkothetiger@gmail.com>
2023-02-13 13:36:09 +0000

    Strict Query set to false

--- scaffold/index.js
+++ scaffold/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
 const mongoose = require('mongoose');
-const dbEndpoint = 'mongobg://127.0.0.1:27017/'
+const dbEndpoint = 'mongodb://127.0.0.1:27017/'
 const dbName = 'scaffold'
 
 const routes = require('./routes');
@@ -18,6 +18,7 @@ app.use('/static', express.static('public'));
 app.use(express.urlencoded({extended: false}));
 app.use(routes);
 
+mongoose.set('strictQuery', false);
 mongoose.connect(dbEndpoint + dbName);
 
 app.listen(3000, () => console.log('Server is running on port 3000..'))

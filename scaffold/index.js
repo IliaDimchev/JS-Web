5561ace4ad0d0417087c1d5fcd9df62ef9b6986b
ilko <ilkothetiger@gmail.com>
2023-02-13 15:53:51 +0000

    Minor bug fixed due to cookie-parser paased as a reference to Ejs causing infinite loading loop

--- scaffold/index.js
+++ scaffold/index.js
@@ -17,10 +17,10 @@ app.set('view engine', 'hbs');
 
 app.use('/static', express.static('public'));
 app.use(express.urlencoded({extended: false}));
-app.use(cookieParser);
+app.use(cookieParser());
 app.use(routes);
 
 mongoose.set('strictQuery', false);
 mongoose.connect(dbEndpoint + dbName);
 
-app.listen(3000, () => console.log('Server is running on port 3000..'))
+app.listen(4000, () => console.log('Server is running on port 3000..'))

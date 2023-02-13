c4a43b014a79319c8a41c8a0e33a8f1e7661f5d3
ilko <ilkothetiger@gmail.com>
2023-02-13 15:06:04 +0000

    Added validation for unique username

--- scaffold/services/authService.js
+++ scaffold/services/authService.js
@@ -1,9 +1,16 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
 
+exports.findByUsername = () => User.findOne(username);
+
 exports.register = async (username, email, password, rePass) => {
     if (password !== rePass) {
         throw new Error('Password mismatch!');
+    }
+
+    const existingUser = await this.findByUsername(username);
+    if (existingUser) {
+        throw new Error('User exists!');
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);

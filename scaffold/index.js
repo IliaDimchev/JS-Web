51e5fc9f8b8ce31bca925c4d2b923d919949fdfe
ilko <ilkothetiger@gmail.com>
2023-02-13 10:06:21 +0000

    Handlebars registered as view engine in EJS

--- scaffold/index.js
+++ scaffold/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
 
 const routes = require('./routes');
 
 const app = express();
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: false}));

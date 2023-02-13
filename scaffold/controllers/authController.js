6e5b2b07e3d590e8f3442d69ce176f0016474160
ilko <ilkothetiger@gmail.com>
2023-02-13 15:30:04 +0000

    JWT saved in cookie and passed to the client

--- scaffold/controllers/authController.js
+++ scaffold/controllers/authController.js
@@ -11,6 +11,8 @@ router.post('/login', async (req, res)  => {
 
     const token = await authService.login(email, password);
     
+    res.cookie('auth', token);
+    res.redirect('/');
 });
 
 router.get('/register', (req, res) => {

191d0beb45e25d64e5184e9750893c1778e74b7e
ilko <ilkothetiger@gmail.com>
2023-02-13 14:17:05 +0000

    Register view added to Auth Controller

--- scaffold/controllers/authController.js
+++ scaffold/controllers/authController.js
@@ -2,6 +2,20 @@ const router = require('express').Router();
 
 router.get('/login', (req, res) => {
     res.render('auth/login');
-})
+});
+
+router.post('/login', (req, res)  => {
+    const { email, password } = req.body;
+});
+
+router.get('/register', (req, res) => {
+    res.render('auth/register');
+});
+
+router.post('/register', (req, res)  => {
+    const { username, email, password, rePass } = req.body;
+});
+
+
 
 module.exports = router;

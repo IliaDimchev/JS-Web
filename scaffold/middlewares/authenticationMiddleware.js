0698c22934b1b6cfcda62b34c9350b96c9c79d34
ilko <ilkothetiger@gmail.com>
2023-02-14 10:36:51 +0000

    Decoded token added to req.user

--- scaffold/middlewares/authenticationMiddleware.js
+++ scaffold/middlewares/authenticationMiddleware.js
@@ -5,7 +5,15 @@ exports.authentication = async (req, res, next) => {
     const ttoken = req.cookies['auth'];
 
     if (token) {
+        try {
         const decodedToken = await jwt.verify(token);
+
+        req.user = decodedToken;
+
+        } catch (err) {
+            res.clearCookie('auth');
+            return res.status(401).render('home/4040');
+        }
     }
 
     next();

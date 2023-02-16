9eee3c67e91b3e7c558a66a8ae6c2538e41fff61
ilko <ilkothetiger@gmail.com>
2023-02-16 11:37:07 +0000

    Error Handling added to Create View

--- scaffold/controllers/cryptoController.js
+++ scaffold/controllers/cryptoController.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 const { isAuthorized } = require('../middlewares/authMiddleware');
 const cryptoService = require('../services/cryptoService');
+const { getErrorMessage } = require('../utils/errorUtils');
 
 router.get('/create', isAuthorized, (req, res) => {
     res.render('crypto/create');
@@ -10,9 +11,13 @@ router.get('/create', isAuthorized, (req, res) => {
 router.post('/create', isAuthorized, async (req, res) => {
     const cryptoData = req.body;
 
-    await cryptoService.create(cryptoData);
+    try {
+        await cryptoService.create(cryptoData);
+    } catch (error) {
+        return res.status(400).render('/crypto/create', { error: getErrorMessage(error) });
+    }
 
-    res.redirect('/');
+    res.redirect('/crypto/catalog');
 });
 
 module.exports = router;

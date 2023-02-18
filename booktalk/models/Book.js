d42f50b013f2d9c1a4c7420a41c9f691338c1a82
ilko <ilkothetiger@gmail.com>
2023-02-18 14:41:28 +0000

    Book model created

---
+++ booktalk/models/Book.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        minLength: 10,
+        required: true,
+    },
+    author: {
+        type: String,
+        minLength: 10,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+        validate: /^http?s:\/\//,
+    },
+    bookReview: {
+        type: String,
+        minLength: 10,
+        required: true,
+    },
+    genre: {
+        type: String,
+        minLength: 10,
+        required: true,
+    },
+    stars: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true,
+    },
+    owner: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+    },
+    wishlist: [{
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+    }],
+});
+
+const Book = mongoose.model('Book', bookSchema);
+
+module.exports = Book;

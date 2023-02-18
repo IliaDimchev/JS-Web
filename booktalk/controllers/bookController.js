b87732d600ead26786a147dd639318e05d0257c2
ilko <ilkothetiger@gmail.com>
2023-02-18 14:35:53 +0000

    Catalog view implemented

--- booktalk/controllers/bookController.js
+++ booktalk/controllers/bookController.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 router.get('/catalog', async (req, res) => {
     // const crypto = await cryptoService.getAll();
-    const books = {}
+    const books = []
 
     res.render('books/catalog', { books });
 });

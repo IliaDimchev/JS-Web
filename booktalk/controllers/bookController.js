ac988ecc9f997561f9b0188946f8430debfcd5fe
ilko <ilkothetiger@gmail.com>
2023-02-18 14:48:13 +0000

    Create static view added

--- booktalk/controllers/bookController.js
+++ booktalk/controllers/bookController.js
@@ -1,10 +1,15 @@
 const router = require('express').Router();
+const { isAuthorized } = require('../middlewares/authMiddleware');
 
 router.get('/catalog', async (req, res) => {
     // const crypto = await cryptoService.getAll();
     const books = []
 
     res.render('books/catalog', { books });
+});
+
+router.get('/create', isAuthorized, (req, res) => {
+    res.render('books/create');
 });
 
 module.exports = router;

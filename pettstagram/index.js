91c90268d5ede192dffa4a6e5e46ec3ab71cf3e1
ilko <ilkothetiger@gmail.com>
2023-02-19 12:01:02 +0000

    Exam scaffold deployed

---
+++ pettstagram/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const handlebars = require('express-handlebars');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const { db } = require('./constants');
+
+
+const routes = require('./routes');
+const { authentication } = require('./middlewares/authMiddleware');
+
+const app = express();
+
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
+
+app.set('view engine', 'hbs');
+
+app.use('/static', express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(authentication);
+app.use(routes);
+
+mongoose.set('strictQuery', false);
+mongoose.connect( db.endpoint + db.name );
+
+app.listen(3000, () => console.log('Server is running on port 3000..'))

42127d54940c48b95415aacfab3b371e515fb64a
ilko <ilkothetiger@gmail.com>
2023-02-19 08:43:22 +0000

    Details view renders static data

--- auction/controllers/auctionController.js
+++ auction/controllers/auctionController.js
@@ -3,12 +3,24 @@ const { isAuthorized } = require('../middlewares/authMiddleware');
 const { getCategoryViewData } = require('../utils/viewDataUtils');
 const auctionService = require('../services/auctionService');
 const { getErrorMessage } = require('../utils/errorUtils');
+const { categoryMap } = require('../constants');
 
 router.get('/catalog', async (req, res) => {
     const auctions = await auctionService.getAll();
 
 
     res.render('auction/catalog', { auctions });
+});
+
+router.get('/catalog/:auctionId/details', async (req, res) => {
+    const auction = await auctionService.getOne(req.params.auctionId);
+
+    const isAuthor = auction.author.toString() === req.user?._id;
+    const isBidder = auction.bidders?.some(id => id == req.user?._id);
+
+    auction.category = categoryMap[auction.category];
+    
+    res.render('auction/details', { auction, isAuthor, isBidder });
 });
 
 router.get('/create', isAuthorized, (req, res) => {
--- auction/services/auctionService.js
+++ auction/services/auctionService.js
@@ -2,5 +2,7 @@ const Auction = require('../models/Auction');
 
 exports.getAll = () => Auction.find({}).lean();
 
+exports.getOne = (auctionId) => Auction.findById(auctionId).lean();
+
 
 exports.create = (author, bookData) => Auction.create({ ...bookData, author });

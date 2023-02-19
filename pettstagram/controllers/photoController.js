9d714a7eeef36b00526328d25473421895bc1dbc
ilko <ilkothetiger@gmail.com>
2023-02-19 13:48:52 +0000

    Edit view implemented

--- pettstagram/controllers/photoController.js
+++ pettstagram/controllers/photoController.js
@@ -10,6 +10,30 @@ router.get('/catalog', async (req, res) => {
     res.render('photo/catalog', { photos });
 });
 
+router.get('/catalog/:photoId/details', async (req, res) => {
+    const photo = await photoService.getOne(req.params.photoId).populate('owner');
+
+    const isOwner = photo.owner._id == req.user?._id;
+    // const isBuyer = photo.buyers?.some(id => id == req.user?._id);
+    const canComment = req.user._id != photo.owner._id
+    photo.comments = [];
+    res.render('photo/details', { photo, isOwner, canComment });
+});
+
+router.get('/catalog/:photoId/edit', isAuthorized, async (req, res) => {
+    const photo = await photoService.getOne(req.params.photoId);
+
+    res.render('photo/edit', { photo });
+});
+
+router.post('/catalog/:photoId/edit', isAuthorized, async (req, res) => {
+    const photoData = req.body;
+
+    await photoService.edit(req.params.photoId, photoData);
+
+    res.redirect(`/catalog/${req.params.photoId}/details`);
+});
+
 router.get('/create', isAuthorized, (req, res) => {
     res.render('photo/create');
 });
--- pettstagram/services/photoService.js
+++ pettstagram/services/photoService.js
@@ -2,4 +2,9 @@ const Photo = require('../models/Photo');
 
 exports.getAll = () => Photo.find({}).lean();
 
+exports.getOne = (photoId) => Photo.findById(photoId).lean();
+
+
 exports.create = (ownerId, photoData) => Photo.create({ ...photoData, owner: ownerId });
+
+exports.edit = (photoId, photoData) => Photo.findByIdAndUpdate(photoId, photoData, { runValidators: true });

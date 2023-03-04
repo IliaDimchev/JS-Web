51e8598ed29fbf1f36c7f4ce5c15622917fdc1a0
ilko <ilkothetiger@gmail.com>
2023-03-04 11:58:52 +0000

    Global CSS added

--- react/workshop/client/src/App.js
+++ react/workshop/client/src/App.js
@@ -1,6 +1,7 @@
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { Search } from "./components/Search";
+import './App.css';
 
 function App() {
   return (

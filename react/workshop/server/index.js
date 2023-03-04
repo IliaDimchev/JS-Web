390c10543f936ebd208a3f301f55ae7122ab1f11
ilko <ilkothetiger@gmail.com>
2023-03-04 12:58:17 +0000

    CORS fetching allowed

--- react/workshop/server/index.js
+++ react/workshop/server/index.js
@@ -8,6 +8,16 @@ const { port, dbConnection } = require('./config/config');
 const allowed = ['.js', '.css', '.png', '.jpg', '.jpeg', '.ico'];
 
 const app = express();
+
+const cors = require("cors");
+const corsOptions = {
+  origin: '*',
+  credentials: true,            //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions))
+
 const start = async () => {
   try {
     await db(dbConnection);

868a5ceb61fffb59ea12635f402784a35256771e
ilko <ilkothetiger@gmail.com>
2023-03-04 13:01:23 +0000

    UserService added destructuring for Object of Objects

--- react/workshop/client/src/services/userService.js
+++ react/workshop/client/src/services/userService.js
@@ -4,5 +4,8 @@ export const getAll = async () => {
     const response = await fetch(baseUrl);
     const result = await response.json();
 
+    // if the server returns Object of objects
+    // return Object.values(result.users);
     return result.users;
+};
 };

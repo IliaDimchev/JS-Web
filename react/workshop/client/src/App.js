118cb102d731ef1efbc6d0f0ed6d0d50a35ea523
ilko <ilkothetiger@gmail.com>
2023-03-04 12:36:23 +0000

    Attempt to useEffect with async function

--- react/workshop/client/src/App.js
+++ react/workshop/client/src/App.js
@@ -1,3 +1,7 @@
+import { useEffect } from 'react';
+
+import * as userService from './services/userService';
+
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { Search } from "./components/Search";
@@ -5,6 +9,10 @@ import './App.css';
 import { UserList } from "./components/UserList";
 
 function App() {
+  useEffect(async = () =>{
+    await userService.getAll();
+  });
+
   return (
     <>
       <Header />

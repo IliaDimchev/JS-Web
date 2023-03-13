7c95416f6f8329fc97990a1030f5a7eafbae0b78
ilko <ilkothetiger@gmail.com>
2023-03-13 21:56:03 +0000

    UserService import fixed

--- react/workshop/client/src/components/User/UserList.js
+++ react/workshop/client/src/components/User/UserList.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-import * as userService from '../services/userService';
+import * as userService from '../../services/userService';
 
 import { User } from "./User/User";
 import { UserDetails } from "./User/UserDetails";

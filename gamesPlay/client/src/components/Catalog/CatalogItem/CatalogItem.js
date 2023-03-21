209e264aab0531d64a0ebd511ddc6e53ab861f20
ilkoTheTiger <ilkothetiger@gmail.com>
2023-03-21 21:52:22 +0200

    CatalogItem props naming fixed

--- gamesPlay/client/src/components/Catalog/CatalogItem/CatalogItem.js
+++ gamesPlay/client/src/components/Catalog/CatalogItem/CatalogItem.js
@@ -1,14 +1,14 @@
 export const CatalogItem = ({
-    name,
+    title,
     imageUrl,
-    genre   
+    category   
 }) => {
     return (
         <div className="allGames">
             <div className="allGames-info">
                 <img src={imageUrl} />
-                <h6>{genre}</h6>
-                <h2>{name}</h2>
+                <h6>{category}</h6>
+                <h2>{title}</h2>
                 <a href="#" className="details-button">Details</a>
             </div>
         </div>

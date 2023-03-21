e02c28a7cb444ee1b35842c5cc7528c06518ee33
ilkoTheTiger <ilkothetiger@gmail.com>
2023-03-21 21:00:28 +0200

    Partial application applied to the requester

--- gamesPlay/client/src/services/requester.js
+++ gamesPlay/client/src/services/requester.js
@@ -1,4 +1,4 @@
-export const request = async (method, url) => {
+const request = async (method, url) => {
     const response = await fetch(url, {
         method,
     });
@@ -12,3 +12,9 @@ export const request = async (method, url) => {
     }
 
 };
+
+export const get = request.bind(null, 'GET');
+export const post = request.bind(null, 'POST');
+export const put = request.bind(null, 'PUT');
+export const patch = request.bind(null, 'PATCH');
+export const del = request.bind(null, 'DELETE');

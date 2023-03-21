f9c7b420b86a48b9be6886972d91104a545a5d14
ilkoTheTiger <ilkothetiger@gmail.com>
2023-03-21 18:17:22 +0200

    Sections spreaded into components

--- gamesPlay/client/src/App.js
+++ gamesPlay/client/src/App.js
@@ -1,9 +1,24 @@
+import { Catalog } from "./components/Catalog/Catalog";
+import { Create } from "./components/Create/Create";
+import { Details } from "./components/Details/Details";
+import { Edit } from "./components/Edit/Edit";
+import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
+import { Home } from "./components/Home/Home";
+import { Login } from "./components/Login/Login";
+import { Register } from "./components/Register/Register";
+
 
 function App() {
     return (
         <div id="box">
             <Header />
+
+            <main id="main-content">
+                
+            </main>
+
+            <Footer />
         </div>
     );
 }

0d45d040d1eba7bf701761910bc3226da062ee93
ilkoTheTiger <ilkothetiger@gmail.com>
2023-03-31 09:49:12 +0300

    Login shortedend in authService

--- gamesPlay/client/src/services/authService.js
+++ gamesPlay/client/src/services/authService.js
@@ -2,6 +2,4 @@ import * as request from './requester';
 
 baseUrl = 'http://localhost:3030/users';
 
-export const login = (loginData) => {
-    request.post(`${baseUrl}/login`,loginData);
-};
+export const login = (loginData) => request.post(`${baseUrl}/login`,loginData);

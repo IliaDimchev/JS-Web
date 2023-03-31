cef90abb8479ec6955d799dc27cd7625cab831bc
ilkoTheTiger <ilkothetiger@gmail.com>
2023-03-31 13:19:41 +0300

    Register form controlled

--- gamesPlay/client/src/components/Register/Register.js
+++ gamesPlay/client/src/components/Register/Register.js
@@ -14,19 +14,38 @@ export const Register = () => {
 
     return (
         <section id="register-page" className="content auth">
-            <form id="register">
+            <form id="register" method="POST" onSubmit={onSubmit}>
                 <div className="container">
                     <div className="brand-logo"></div>
                     <h1>Register</h1>
 
                     <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" placeholder="maria@email.com" />
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        placeholder="maria@email.com"
+                        value={values.email}
+                        onChange={changeHandler}
+                     />
 
                     <label htmlFor="pass">Password:</label>
-                    <input type="password" name="password" id="register-password" />
+                    <input
+                        type="password"
+                        name="password"
+                        id="register-password"
+                        value={values.password}
+                        onChange={changeHandler}
+                    />
 
                     <label htmlFor="con-pass">Confirm Password:</label>
-                    <input type="password" name="confirmPassword" id="confirm-password" />
+                    <input
+                        type="password"
+                        name="confirmPassword"
+                        id="confirm-password"
+                        value={values.confirmPassword}
+                        onChange={changeHandler}
+                    />
 
                     <input className="btn submit" type="submit" value="Register" />
 

8c8ff2a66f65f5dfd06352407a213e0be3797d32
ilkoTheTiger <ilkothetiger@gmail.com>
2023-03-31 12:35:21 +0300

    Navigation reacts based on authentication

--- gamesPlay/client/src/App.js
+++ gamesPlay/client/src/App.js
@@ -53,9 +53,7 @@ function App() {
         userId: auth._id,
         token: auth.accessToken,
         userEmail: auth.email,   
-        isAuthenticated() {
-            return !!auth.accessToken
-        },
+        isAuthenticated: !!auth.accessToken,
     };
 
     return (

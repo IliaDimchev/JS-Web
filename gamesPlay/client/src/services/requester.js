fb79fa3728606d869ce42c9b77ecc6c9d24178aa
ilkoTheTiger <ilkothetiger@gmail.com>
2023-04-03 10:28:28 +0300

    Requester refactored

--- gamesPlay/client/src/services/requester.js
+++ gamesPlay/client/src/services/requester.js
@@ -1,4 +1,4 @@
-const request = async (method, url, data) => {
+const request = async (method, token, url, data) => {
     const options = {};
 
     if (method !== 'GET') {
@@ -11,25 +11,29 @@ const request = async (method, url, data) => {
             options.body = JSON.stringify(data);
         };
     };
-    
+
     const response = await fetch(url, options);
-        
+
     if (response.status === 204) {
         return {};
     }
 
     const result = await response.json();
-    
-    if (!response.ok) {        
+
+    if (!response.ok) {
         throw result;
-    }    
+    }
 
     return result;
 
 };
 
-export const get = request.bind(null, 'GET');
-export const post = request.bind(null, 'POST');
-export const put = request.bind(null, 'PUT');
-export const patch = request.bind(null, 'PATCH');
-export const del = request.bind(null, 'DELETE');
+export const requestFactory = (token) => {
+    return {
+        get: request.bind(null, 'GET', token),
+        post: request.bind(null, 'POST', token),
+        put: request.bind(null, 'PUT', token),
+        patch: request.bind(null, 'PATCH', token),
+        delete: request.bind(null, 'DELETE', token),
+    }
+};

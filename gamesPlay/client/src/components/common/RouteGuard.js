7061ff9e802c586d18ae44e850ecbd4ba5de09fc
ilkoTheTiger <ilkothetiger@gmail.com>
2023-04-07 09:28:55 +0300

    RouteGuard redirects to login when unathorized users tries to access create view

--- gamesPlay/client/src/components/common/RouteGuard.js
+++ gamesPlay/client/src/components/common/RouteGuard.js
@@ -1,8 +1,16 @@
+import { Navigate } from "react-router-dom";
 
+import { useAuthContext } from "../../contexts/AuthContext";
 
 export const RouteGuard = ({
     children,
 }) => {
+    const { isAuthenticated } = useAuthContext();
+
+    if (!isAuthenticated) {
+        return <Navigate to='/login' />;
+    }
+
     return (
         <>
             {children}
